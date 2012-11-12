cb05f2b7d66e6013798c382c5ebf6ed3e775f0a1
Lawrence Peach <vf30wrx@gmail.com>
2012-11-12 07:24:05 -0500

    Update aocp.mk
    rename some things

--- aocp.mk
+++ aocp.mk
@@ -1,18 +1,3 @@
-#
-# Copyright (C) 2012 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 # Release name
 PRODUCT_RELEASE_NAME := SPH-D710
@@ -25,7 +10,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         vendor/aocp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation.zip
 
-# Inherit some common CM stuff.
+# Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/configs/common_phone.mk)
 
 # Inherit device configuration
